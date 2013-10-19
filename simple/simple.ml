cb3e56c36fd58791794c666fd39627fa7130f6eb
Kensuke Matsuzaki <zakki@peppermint.jp>
2013-10-20 04:14:14 +0900

    Add ispc example port

---
+++ simple/simple.ml
@@ -0,0 +1,34 @@
+open Ctypes
+open PosixTypes
+open Foreign
+
+let lib = Dl.(dlopen ~filename:"./simple.so" ~flags:[RTLD_NOW])
+
+let s = foreign "simple"
+  (ptr float @-> ptr float @-> int32_t @-> returning void)
+  ~from:lib
+
+let simple' vin vout count =
+  let vin' = Array.start vin in
+  let vout' = Array.start vout in
+  let count' = Int32.of_int count in
+  s vin' vout' count'
+
+let _ =
+  (* Initialize input buffer *)
+  let vin = Array.make float 16 in
+  for i = 0 to 15 do
+    Array.set vin i (float_of_int i)
+  done;
+  let vout = Array.make float 16 in
+
+  (* Call simple() function from simple.ispc file *)
+  simple' vin vout 16;
+
+  (* Print results *)
+  for i = 0 to 15 do
+    Printf.printf "%d: simple(%f) = %f\n" i
+      (Array.get vin i)
+      (Array.get vout i);
+  done;
+  ()
